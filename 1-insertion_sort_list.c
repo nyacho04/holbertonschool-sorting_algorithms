b468ddd3bebf940cec5409b3f9049178afe658c3
nyacho04 <dnachod83@gmail.com>
2024-12-16 02:57:34 +0000

    commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,32 +7,32 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *current, *temp;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+	if (!list || !(*list) || !(*list)->next)
+	return;
 
-    current = (*list)->next;
-    while (current)
-    {
-        temp = current;
-        while (temp->prev && temp->n < temp->prev->n)
-        {
-            temp->prev->next = temp->next;
-            if (temp->next)
-                temp->next->prev = temp->prev;
+	current = (*list)->next;
+	while (current)
+	{
+	temp = current;
+	while (temp->prev && temp->n < temp->prev->n)
+	{
+		temp->prev->next = temp->next;
+		if (temp->next)
+		temp->next->prev = temp->prev;
 
-            temp->next = temp->prev;
-            temp->prev = temp->next->prev;
-            temp->next->prev = temp;
+		temp->next = temp->prev;
+		temp->prev = temp->next->prev;
+		temp->next->prev = temp;
 
-            if (temp->prev)
-                temp->prev->next = temp;
-            else
-                *list = temp;
+		if (temp->prev)
+		temp->prev->next = temp;
+	else
+	*list = temp;
 
-            print_list(*list);
-        }
-        current = current->next;
-    }
+	print_list(*list);
+	}
+	current = current->next;
+	}
 }
